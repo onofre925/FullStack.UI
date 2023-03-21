9c3fb3e651c94316dbb9df7687a7d73ecfbbabf3
Emanuel Onofre <emanuelonofre@csus.edu>
2023-03-20 23:06:37 -0700

    Call Web API from Angular app pt 8

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
@@ -8,37 +9,19 @@ import { Employee } from 'src/app/models/employee.model';
 })
 export class EmployeesListComponent implements OnInit {
 
-  employees: Employee[] = [
-    {
-      id: 'c705cd8b-0297-441c-af5b-102620816a70',
-      name:'Manny Gonzalez',
-      email: 'mannyG@gmail.com',
-      phone: 619877665,
-      salary: 70000,
-      department: 'Information Technology'
-    },
-    {
-      id: 'c705cd8b-0297-441c-af5b-102620816a70',
-      name:'Sammantha Hudson',
-      email:'sammyH@gmail.com',
-      phone: 916877665,
-      salary: 70000,
-      department: 'Information Technology'
-    },
-    {
-      id: 'c705cd8b-0297-441c-af5b-102620816a70',
-      name:'Chloe Gallagher',
-      email: 'chloeH@gmail.com',
-      phone: 925877665,
-      salary: 70000,
-      department: 'Information Technology'
-    }
-  
-  ];
-  constructor() { }
+  employees: Employee[] = [];
+  constructor(private employeesService: EmployeesService) { }
 
   ngOnInit(): void {
-
+    this.employeesService.getAllEmployees()
+    .subscribe({
+      next: (employees) => {
+        console.log(employees);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    })
 
   }
 
